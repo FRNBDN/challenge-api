7e5edddcec57c4e0f183ed9daaedd960b947629a
FRNBDN <francesco.badini94@gmail.com>
2023-03-10 04:39:01 +0000

    update move challenges fields to challenge model

--- challenges/models.py
+++ challenges/models.py
@@ -1,18 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import date
 from groups.models import Group
+
+
+REPETITION_CHOICES = (
+    ('never', 'One Time Challenge'),
+    ('daily', 'Repeats Daily'),
+    ('weekly', 'Repeats Weekly'),
+    ('monthly', 'Repeats Monthly'),
+    ('annualy', 'Repeats Annualy'),
+)
 
 
 class Challenge(models.Model):
     group = models.OneToOneField(Group, on_delete=models.CASCADE)
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=160)
-    number = models.CharField(max_length=16)
-    date = models.DateField()
+    title = models.CharField(max_length=160, default="Challenge")
+    description = models.TextField(max_length=500, default='...')
+    criteria = models.TextField(max_length=500, default='...')
+    date = models.DateField(default=date.today, blank=False)
+    repetition = models.CharField(
+        max_length=25, choices=REPETITION_CHOICES, default='weekly')
+    end_date = models.DateField(blank=True, null=True)
 
     class Meta:
         ordering = ['-created_at']
 
     def __str__(self):
-        return f"{self.title} #{self.number}"
+        return f"({self.id}) {self.title}"
--- groups/models.py
+++ groups/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from taggit.managers import TaggableManager
-from datetime import date
+
 
 CATEGORY_CHOICES = (
     ('SPI', 'Spiritual'),
@@ -13,14 +13,6 @@ CATEGORY_CHOICES = (
     ('ETC', 'Other'),
 )
 
-REPETITION_CHOICES = (
-    ('never', 'One Time Challenge'),
-    ('daily', 'Repeats Daily'),
-    ('weekly', 'Repeats Weekly'),
-    ('monthly', 'Repeats Monthly'),
-    ('annualy', 'Repeats Annualy'),
-)
-
 
 class Group(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -30,18 +22,10 @@ class Group(models.Model):
     category = models.CharField(max_length=25,
                                 choices=CATEGORY_CHOICES,
                                 default='ETC')
-    challenge_title = models.CharField(max_length=160, default="Challenge")
-    description = models.TextField(max_length=500, default='...')
-    criteria = models.TextField(max_length=500, default='...')
-    date = models.DateField(default=date.today, blank=False)
-    repetition = models.CharField(
-        max_length=25, choices=REPETITION_CHOICES, default='weekly')
-    end_date = models.DateField(blank=True, null=True)
     tags = TaggableManager(blank=True)
-    # memebers and challenge missing
 
     class Meta:
         ordering = ['-created_at']
 
     def __str__(self):
-        return f"[{self.category}] {self.title} ({self.id})"
+        return f" ({self.id}) [{self.category}] {self.title}"
