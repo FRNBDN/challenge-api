1672bcd550a29b07b1df0adc0c33e424b1a2bcd8
FRNBDN <francesco.badini94@gmail.com>
2023-03-13 13:55:52 +0000

    add commend field to submissions

--- submissions/serializers.py
+++ submissions/serializers.py
@@ -1,17 +1,29 @@
 from rest_framework import serializers
 from .models import Submission
+from commends.models import Commend
 
 
 class SubmissionSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    commend_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_commend_id(self, obj):
+        request = self.context['request'].user
+        if user.is_authenticated:
+            commend = Commend.objects.filter(
+                owner=user, submission=obj
+            ).first()
+            return commend.id if commend else None
+        return None
+
     class Meta:
         model = Submission
         fields = [
             'id', 'owner', 'group', 'challenge', 'text', 'is_owner',
+            'commend_id',
         ]
