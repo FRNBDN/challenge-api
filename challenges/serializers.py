54c2f6e7b4ba132f08c558e10498dddc3f36bb20
FRNBDN <francesco.badini94@gmail.com>
2023-03-13 13:55:35 +0000

    add criteria field to challenges

--- challenges/serializers.py
+++ challenges/serializers.py
@@ -1,14 +1,26 @@
 from rest_framework import serializers
 from .models import Challenge
+from criteria.models import Criteria
 
 
 class ChallengeSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    criteria_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_criteria_id(self, obj):
+        request = self.context['request'].user
+        criteria = Criteria.objects.filter(
+            challenge=obj
+            )
+        criterionList = []
+        for criterion in criteria:
+            criterionList.append(criterion.id)
+        return criterionList
 
     class Meta:
         model = Challenge
