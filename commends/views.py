e39cc9dfb14cdf9b00331ca75f5f7437b489c257
FRNBDN <francesco.badini94@gmail.com>
2023-03-13 12:34:03 +0000

    add commendlist views

--- commends/views.py
+++ commends/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permission import IsOwnerOrReadOnly
+from .models import Commend
+from .serializers import CommendSerializer
 
-# Create your views here.
+
+class CommendList(generics.ListCreateAPIView):
+    serializer_class = CommendSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Commend.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
