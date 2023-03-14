79f9be57fd9acad979baa7b4fdb252be65547683
FRNBDN <francesco.badini94@gmail.com>
2023-03-14 13:36:26 +0000

    add filters to groups, profiles

--- groups/views.py
+++ groups/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Group
 from .serializers import GroupSerializer
 from drf_api.permissions import IsOwnerOrReadOnly
@@ -19,6 +20,12 @@ class GroupList(generics.ListCreateAPIView):
     filter_backends = [
         filters.OrderingFilter,
         filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'owner__profile',
+
     ]
     search_fields = [
         'owner__username',
