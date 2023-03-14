82d25655c3c8d40734ef57cc56d87336d214f446
FRNBDN <francesco.badini94@gmail.com>
2023-03-14 09:10:39 +0000

    add status field to submissions model

--- submissions/models.py
+++ submissions/models.py
@@ -3,9 +3,19 @@ from django.contrib.auth.models import User
 from groups.models import Group
 from challenges.models import Challenge
 
+SUBMISSION_STATUS = (
+    (1, 'Submitted'),
+    (2, 'Completed'),
+    (3, 'Failed'),
+)
+
 
 class Submission(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     group = models.OneToOneField(Group, on_delete=models.CASCADE)
     challenge = models.OneToOneField(Challenge, on_delete=models.CASCADE)
     text = models.TextField(max_length=500)
+    stauts = models.CharField(
+        max_length=50,
+        choices=SUBMISSION_STATUS,
+        default=1)
